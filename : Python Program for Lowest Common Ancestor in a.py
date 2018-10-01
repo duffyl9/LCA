034a6090b9c741f27dfee4e13f38d1ced529cc7b
Lauren <duffyl9@tcd.ie>
2018-10-01 16:18:45 +0100

    Added all files and first working test

--- : Python Program for Lowest Common Ancestor in a.py
+++ : Python Program for Lowest Common Ancestor in a.py
@@ -73,6 +73,5 @@ print "LCA(4, 5) = %d" %(findLCA(root, 4, 5,))
 print "LCA(4, 6) = %d" %(findLCA(root, 4, 6)) 
 print "LCA(3, 4) = %d" %(findLCA(root,3,4)) 
 print "LCA(2, 4) = %d" %(findLCA(root,2, 4)) 
-print "LCA(1, 2) = %d" %(findLCA(root,1, 2)) 
 
 # This code is contributed by Nikhil Kumar Singh(nickzuck_007) 
