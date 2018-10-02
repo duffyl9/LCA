f32b26fea5dd11937a9d30ea5d1e514dd7e74e1e
Lauren <duffyl9@tcd.ie>
2018-10-02 13:01:16 +0100

    added second working test

--- TestLCA.py
+++ TestLCA.py
@@ -1,6 +1,6 @@
 import unittest
+import LCA
 from LCA import findPath 
-from LCA import __init__ 
 from LCA import Node 
 from LCA import findLCA
 from LCA import findPath
@@ -8,11 +8,43 @@ from LCA import findPath
 
 class TestLCA(unittest.TestCase):
 
-	def testNodes(self):
+	def testNodes(self): #Testing root value in the tree
 
 		root = Node(1)
-		actualNodeOne = Node(root)
 		self.assertEqual(1, root.key)
+
+	def testFindLCA(self):
+
+		root = Node(1)
+		root.left = Node(2)
+		root.right = Node(3)
+		root.left.left = Node(4)
+		root.left.right = Node(5)
+		root.right.left = Node(6)
+		root.right.right = Node(7)
+
+		# test all nodes are None 
+		self.assertEqual(LCA.findLCA(None, None, None), -1)
+
+		#test when root is euqal to node
+		root = Node(1)
+		self.assertEqual(LCA.findLCA(root, 1, 1), 1)
+
+		#test if n2 is not present
+		self.assertEqual(LCA.findLCA(root, 2, None), -1)
+
+		#test when node is value is not in tree
+		self.assertEqual(LCA.findLCA(root, 2, 13), -1)
+
+		#test LCA for different values
+		#self.assertAlmostEqual(LCA.findLCA(root, 3, 7), 3)
+		#self.assertEqual(LCA.findLCA(root, 4, 6), 1)
+		#self.assertEqual(LCA.findLCA(root, 2, 5), 2)
+		#self.assertEqual(findLCA(root, 3, 4), 1)
+
+
+
+
 
 
 if __name__ == '__main__':
