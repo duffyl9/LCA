b1da52dbd68a79821575f39afbeec16d03d3adc9
Lauren <duffyl9@tcd.ie>
2018-10-16 15:19:02 +0100

    Added new tests for findLCA function

--- TestLCA.py
+++ TestLCA.py
@@ -1,7 +1,8 @@
 import unittest
 import LCA
+from LCA import findLCAUtil
 from LCA import findLCA
-from LCA import findPath 
+from LCA import find 
 from LCA import Node 
 
 
@@ -23,55 +24,26 @@ class TestLCA(unittest.TestCase):
 		root.right.right = Node(7)
 
 		# test all nodes are None 
-		self.assertEqual(LCA.findLCA(None, None, None), -1)
+		self.assertEqual(LCA.findLCA(None, None, None), None)
 
 		#test when root is euqal to node
-		self.assertEqual(LCA.findLCA(root, 1, 1), 1)
+		self.assertEqual(LCA.findLCA(root, 1, 1).key, 1)
 
 		#test if n1 is not present
-		self.assertEqual(LCA.findLCA(root, None, 2), -1)
+		self.assertEqual(LCA.findLCA(root, None, 2), None)
 
 		#test if n2 is not present
-		self.assertEqual(LCA.findLCA(root, 2, None), -1)
+		self.assertEqual(LCA.findLCA(root, 2, None), None)
 
 		#test when node is value is not in tree
-		self.assertEqual(LCA.findLCA(root, 2, 13), -1)
+		self.assertEqual(LCA.findLCA(root, 2, 13), None)
 
 		#test LCA for different values
-		self.assertEqual(findLCA(root, 3, 7), 3)
-		self.assertEqual(findLCA(root, 4, 6), 1)
-		self.assertEqual(findLCA(root, 2, 5), 2)
-		self.assertEqual(findLCA(root, 3, 4), 1)
+		self.assertEqual(findLCA(root, 3, 7).key, 3)
+		self.assertEqual(findLCA(root, 4, 6).key, 1)
+		self.assertEqual(findLCA(root, 2, 5).key, 2)
+		self.assertEqual(findLCA(root, 3, 4).key, 1)
 
-	def testFindPath(self):
-
-		root = Node(1)
-		root.left = Node(2)
-		root.right = Node(3)
-		root.left.left = Node(4)
-		root.left.right = Node(5)
-		root.right.left = Node(6)
-		root.right.right = Node(7)
-
-		path = []
-		k = 1
-
-		#Test for if k == root's key
-		k = 1
-		self.assertTrue(findPath(root, path, k))
-
-		#Test for if k is present in left or right sub-tree
-		k = 4
-		self.assertTrue(findPath(root, path, k))
-
-		#Test for if k is not present in subtree rooted with root
-		k = 8
-		self.assertFalse(findPath(root, path, k))
-
-		#Test for when root is None
-		root = None
-		k = 3
-		self.assertFalse(findPath(root, path, k))
 
 
 if __name__ == '__main__':
