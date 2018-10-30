f1bcc9e5e2fda6e283ef50dbd54dfd9a9c8979cc
Lauren <duffyl9@tcd.ie>
2018-10-30 15:41:46 +0000

    Added new tests for DAG

--- Tests_v2.py
+++ Tests_v2.py
@@ -1,9 +1,49 @@
 import unittest
 import dag
+from dag import findLCA
+from dag import findPathsTo
+from dag import pathTo
+from dag import Node
 
 class test_dag(unittest.TestCase):
 
-    def test_findLCA(self):
+    def testNodes(self): #Testing root value in the tree
+
+        root = Node(1)
+        self.assertEqual(1, root.key)
+
+    def testFindLCA(self):
+
+        root = Node(1)
+        root.children.append(dag.Node(2)) 
+        root.children.append(dag.Node(3))
+        root.children[0].children.append(dag.Node(4))
+        root.children[0].children.append(dag.Node(5))
+        root.children[1].children.append(dag.Node(6))
+        root.children[1].children.append(dag.Node(7)) 
+
+        # test all nodes are None 
+        self.assertEqual(dag.findLCA(None, None, None), None)
+
+        #test when root is euqal to node
+        self.assertEqual(dag.findLCA(root, 1, 1), 1)
+
+        #test if n1 is not present
+        self.assertEqual(dag.findLCA(root, None, 2), None)
+
+        #test if n2 is not present
+        self.assertEqual(dag.findLCA(root, 2, None), None)
+
+        #test when node is value is not in tree
+        self.assertEqual(dag.findLCA(root, 2, 13), None)
+
+        #test LCA for different values
+        self.assertEqual(dag.findLCA(root, 3, 7), 3)
+        self.assertEqual(dag.findLCA(root, 4, 6), 1)
+        self.assertEqual(dag.findLCA(root, 2, 5), 2) 
+        self.assertEqual(dag.findLCA(root, 3, 4), 1)
+
+    def test_findLCA_v2(self):
         #Test 1: test all values equal None
         self.assertEqual(dag.findLCA(None, None, None), None)
 
@@ -14,7 +54,7 @@ class test_dag(unittest.TestCase):
         #Test 3: test when a value is not in tree
         self.assertEqual(dag.findLCA(root, 50, 51), None)
 
-        #Test 4: Test DAG with 7 nodes
+        #Test 4: Test dag with 7 nodes
         root.children.append(dag.Node(2)) 
         root.children.append(dag.Node(3))
         root.children[0].children.append(dag.Node(4))
